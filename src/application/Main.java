0c8464ad6d91cde8014b00e6b569e4ea2537cf68
nandanvinjamury <nvinjamury@gmail.com>
2020-03-31 20:04:40 -0700

    added first scene basic framework

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,20 +1,34 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+
 
 
 public class Main extends Application {
 	@Override
-	public void start(Stage primaryStage) {
+	public void start(Stage stage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
+			
+			Label prompt = new Label("Enter a floating point number for a step by step infographic on IEEE-754 Storage: ");
+			TextField field = new TextField();
+			Button calculate = new Button("Calculate");
+			field.setMaxWidth(300);
+			VBox vbox = new VBox(30, prompt, field, calculate);
+			vbox.setAlignment(Pos.TOP_CENTER);
+			vbox.setPadding(new Insets(30));
+			
+			Scene scene = new Scene(vbox,800,450);
+			stage.setScene(scene);
+			stage.setTitle("IEEE-754 Step By Step Converter");
+			stage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
